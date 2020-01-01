aed41b85c1b26c85f146b2b9adb0befccd91790e
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-01-01 22:09:26 +0300

    add state to Board

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -3,18 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        }
+    }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button 
+      className="square" 
+      onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
